49ddf4dd16200ab6081bd7e6e2035695e50ae123
yakhlafhoussam <yakhlafhoussam@gmail.com>
2025-12-13 23:26:43 +0100

    Link the project data whit Firebase and add same animations

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const speed = 0.4;
 let posX = 0;
 let posY = 0;
 let saj;
+let hs;
 const hyk = document.querySelector("#hyk");
 const observerCallback = entries => {
     entries.forEach(entry => {
@@ -18,6 +19,68 @@ reveals.forEach(section => {
     section.classList.add('opacity-0', 'translate-y-10', 'transition-all', 'duration-700');
     observer.observe(section);
 });
+
+function projectFromFirebase() {
+
+    const slider = document.getElementById("slider");
+    const slides = document.querySelectorAll("#slider > div");
+    const dotsContainer = document.getElementById("dots");
+    let autoSlideTimer;
+
+    let index = 0;
+
+    function updateSlider() {
+        slider.style.transform = `translateX(-${index * 100}%)`;
+
+        dotsContainer.querySelectorAll("button").forEach((dot, i) => {
+            dot.classList.toggle("bg-white", i === index);
+            dot.classList.toggle("bg-gray-600", i !== index);
+        });
+    }
+
+    // Create dots
+    slides.forEach((_, i) => {
+        const dot = document.createElement("button");
+        dot.className =
+            "w-3 h-3 rounded-full bg-gray-600 transition hover:bg-white";
+        dot.onclick = () => {
+            index = i;
+            updateSlider();
+            resetAutoSlide();
+        };
+        dotsContainer.appendChild(dot);
+    });
+
+    document.getElementById("next").onclick = () => {
+        index = (index + 1) % slides.length;
+        updateSlider();
+        resetAutoSlide();
+    };
+
+    document.getElementById("prev").onclick = () => {
+        index = (index - 1 + slides.length) % slides.length;
+        updateSlider();
+        resetAutoSlide();
+    };
+
+    function resetAutoSlide() {
+        clearInterval(autoSlideTimer);
+        startAutoSlide();
+    }
+
+    // Auto slide
+    function startAutoSlide() {
+        autoSlideTimer = setInterval(() => {
+            index = (index + 1) % slides.length;
+            updateSlider();
+        }, 5000);
+    }
+
+    startAutoSlide();
+    updateSlider();
+
+}
+
 
 /**************************************FireBase*************************************** */
 
@@ -47,11 +110,31 @@ const analytics = getAnalytics(app);
 const db = getDatabase(app);
 const contactRef = ref(db, "contacts/");
 const skills = ref(db, "skills/");
+const projects = ref(db, "projects/");
 
 /************************************************************************************** */
 
 // مثال تخزين مصفوفة
-let myArray = ["HTML, CSS, JavaScript, TailwindCSS", "PHP, Firebase, MySQL", "Figma", "Git, GitHub, Trello, Jira, GitHub Projects"];
+let myArray = [
+    {
+        img: "https://maghreb.simplonline.co/_next/image?url=https%3A%2F%2Fsimplonline-v3-prod.s3.eu-west-3.amazonaws.com%2Fmedia%2Fimage%2Fpng%2Fchatgpt-image-nov-16-2025-02-15-39-pm-691a1cfdb1aa0331529428.png&w=1280&q=75",
+        link: "https://yakhlafhoussam.github.io/Brief-Soutenance-Crois-e-1-WorkSphere---Virtual-Workspace-/",
+        title: "WorkSphere",
+        des: "Modern virtual workspace."
+    },
+    {
+        img: "https://yakhlafhoussam.github.io/Brief-2-S2/img/cover.png",
+        link: "https://yakhlafhoussam.github.io/Brief-2-S2/",
+        title: "PixelQuest",
+        des: "Interactive video game catalog."
+    },
+    {
+        img: "https://maghreb.simplonline.co/_next/image?url=https%3A%2F%2Fsimplonline-v3-prod.s3.eu-west-3.amazonaws.com%2Fmedia%2Fimage%2Fpng%2Fchatgpt-image-oct-20-2025-04-26-55-am-68f5ac0ad4d70051267516.png&w=1280&q=75",
+        link: "https://yakhlafhoussam.github.io/Brief-3/",
+        title: "E-Dirham",
+        des: "Moroccan digital currency."
+    },
+];
 
 /* // طريقة التخزين
 set(ref(db, "projects/"), myArray)
@@ -64,7 +147,27 @@ get(skills).then((ajj) => {
     document.querySelector("#back").innerHTML = saj[1];
     document.querySelector("#design").innerHTML = saj[2];
     document.querySelector("#tools").innerHTML = saj[3];
- })
+})
+get(projects).then((ajyk) => {
+   hs = ajyk.val();
+   document.querySelector("#slider").innerHTML = "";
+   for (let saou = 0; saou < hs.length; saou++) {
+       document.querySelector("#slider").insertAdjacentHTML("beforeend", `
+           <div class="min-w-full h-full flex flex-col items-center text-center px-6">
+       <a class="mb-6 w-full h-5/6 max-w-md md:max-w-full" target="_blank" rel="noopener noreferrer" href='${hs[saou].link}'>
+       <img
+         src='${hs[saou].img}'
+         class="w-full h-full object-cover rounded-2xl shadow-lg"
+       /></a>
+       <h3 class="text-2xl font-bold mb-2">${hs[saou].title}</h3>
+       <p class="text-gray-400 max-w-md">
+         ${hs[saou].des}
+       </p>
+     </div>
+           `)
+   }
+   projectFromFirebase();
+})
 
 
 function sendsucces() {
@@ -125,10 +228,12 @@ function enable(e) {
 }
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY >= 920 && window.scrollY <= 950) {
+    if (window.scrollY >= 920 && window.scrollY <= 950 && window.innerWidth >= 1024) {
         document.querySelector('body').addEventListener('wheel', enable, { passive: false });
     }
 });
+
+
 
 document.addEventListener("DOMContentLoaded", event => {
     gsap.to(hyk, {
@@ -137,6 +242,33 @@ document.addEventListener("DOMContentLoaded", event => {
         color: "#000000",
         yoyo: true,
         repeat: -1,
+    });
+
+    const tl = gsap.timeline({ repeat: -1,});
+
+    tl.to("#load1", { color: "#000000", duration: 2.5, ease: "none" })
+        .to("#load2", { color: "#000000", duration: 2.5, ease: "none" }, "-=1.5")
+        .to("#load3", { color: "#000000", duration: 2.5, ease: "none" }, "-=1.5")
+        .to("#load1", { color: "#ffffff", duration: 2.5, ease: "none" }, "-=1.5")
+        .to("#load2", { color: "#ffffff", duration: 2.5, ease: "none" }, "-=1.5")
+        .to("#load3", { color: "#ffffff", duration: 2.5, ease: "none" }, "-=1.5");
+
+    gsap.from("#saj", {
+        duration: 8,
+        color: "#000000",
+        ease: "none",
+    });
+
+    gsap.to("#moon", {
+        duration: 2,
+        ease: "none",
+        opacity: 1,
+    });
+
+    gsap.to("#sun", {
+        duration: 2,
+        ease: "none",
+        opacity: 1,
     });
 
     gsap.to("#moon", {
@@ -151,6 +283,16 @@ document.addEventListener("DOMContentLoaded", event => {
 
     gsap.to("#sun", {
         x: +600,
+        scrollTrigger: {
+            trigger: "body",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: true,
+        },
+    });
+
+    gsap.to("#star", {
+        opacity: 0,
         scrollTrigger: {
             trigger: "body",
             start: "top top",
